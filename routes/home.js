865a50d3302760bba7c9b4a623a04e6366bdc328
lester <servqua88@gmail.com>
2019-11-16 21:12:54 +0800

    Add date and category filter for routes  and views

--- routes/home.js
+++ routes/home.js
@@ -4,19 +4,33 @@ const { authenticated } = require('../config/auth')
 const db = require('../models')
 const User = db.User
 const Record = db.Record
+const months = require('../data/date.json').results
+const categorys = require('../data/category.json').results
+const sequelize = require('sequelize')
+const Op = sequelize.Op
 
 router.get('/', authenticated, (req, res) => {
   let totalAmount = 0
+  const month = req.query.months || ''
+  const category = req.query.categorys || ''
   // console.log('reqUserId', req.user.id)
+  let filtermonth = (month === '') ? {} : { date: { [Op.and]: [sequelize.where(sequelize.fn('MONTH', sequelize.col('date')), parseInt(month))] } }
+  let filtercategory = (category === '') ? {} : { category: category }
+
   User.findByPk(req.user.id).then((user) => {
     if (!user) throw new Error('user not found')
-
-    return Record.findAll({ where: { UserId: req.user.id } }).then((records) => {
+    return Record.findAll({
+      where: {
+        UserId: req.user.id,
+        ...filtercategory,
+        ...filtermonth
+      }
+    }).then((records) => {
       records.forEach((record) => {
         record.dateTime = `${record.date.getFullYear()}/${(record.date.getMonth() + 1)}/${record.date.getDate()}`
         totalAmount += record.amount
       })
-      return res.render('index', { records, totalAmount })
+      return res.render('index', { records, totalAmount, months, month, categorys, category })
     })
   })
     .catch((error) => { res.status(422).json(error) })
